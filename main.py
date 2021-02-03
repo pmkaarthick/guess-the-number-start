7a507cec25cdc835bea48bbccfa7d8e03837b779
Karthick Perumal <pmkaarthick@gmail.com>
2021-02-03 17:38:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+print("Welcome to Number Guessing Game")
+level=input("Enter the difficulty level")
+if(level == "easy"):
+  chances = 10
+else:
+  chances = 5
+
+actual = random.randint(1,101)
+gameover = False
+while not gameover:
+  guess = int(input("Enter any number between 1 to 100"))
+  if(chances>0):
+    if(guess == actual):
+      print("You Won")
+      gameover=True
+    elif guess>actual:
+      print("Too High")
+      chances-=1
+    else:
+      print("Too Low")
+      chances-=1
+  else:
+    print("Game Over")
